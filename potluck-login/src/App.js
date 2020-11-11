e4783f210efaac5ec420bbe026d66cba7c672853
Lakendria <kendra939@gmail.com>
2020-11-10 22:24:38 -0500

    created components

---
+++ potluck-login/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Login from "./components/login"
+import Register from "./components/register"
+
+function App() {
+  return (
+    <div className="App">
+     <Login/>
+     <Register/>
+    </div>
+  );
+}
+
+export default App;
