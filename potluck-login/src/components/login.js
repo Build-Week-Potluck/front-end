7176938dcc0433845bf97321ee981486afa058a5
Lakendria <kendra939@gmail.com>
2020-11-11 22:20:10 -0500

    Login Form created

--- potluck-login/src/components/login.js
+++ potluck-login/src/components/login.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import {Col, Button, Form, FormGroup,Label,Input,Card,CardTitle,CardText,CardSubtitle} from "reactstrap";
 function Login (){
     return (
-        <div>
-            
-        </div>
+        <Card>
+        <Form>
+            <div>
+                <h2>Login</h2>
+                <FormGroup>
+                    <Label htmlFor ="email">Email:</Label>
+                    <Input type="email" name="email" id="email"/>
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="password" name="password">Password:</Label>
+                    <Input type="password" name="password" id="password"/>
+                </FormGroup>
+            </div>
+        </Form>
+        </Card>
     )
 }
 
