f05d7872846776cbd0cdf4bb43c66c65a8b91800
Lakendria <kendra939@gmail.com>
2020-11-11 19:55:27 -0500

    Routed components

--- potluck-login/src/App.js
+++ potluck-login/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import React from 'react';
+import {Route, Link} from "react-router-dom";
 import Login from "./components/login"
 import Register from "./components/register"
 
 function App() {
   return (
     <div className="App">
-     <Login/>
-     <Register/>
+      <nav className="header">
+        <h1 className = "logo"> Potluck Planner</h1>
+        <div className= "links">
+          <Link className ="register" to = "/register"><button>Register</button></Link>
+          <Link className="login" to="/login"><button>Login</button></Link>
+        </div>
+      </nav>
+      <Route path ="/register" component={Register}/>
+      <Route path="/login" component={Login}/>
+  
     </div>
   );
-}
+};
 
 export default App;
